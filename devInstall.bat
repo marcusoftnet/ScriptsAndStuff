1a6100c8ee9e4c342b94bb94156684b3c7312f4c
Marcus Hammarberg <marcusoft.net@gmail.com>
2013-08-01 16:17:11 +0200

    Initial version

---
+++ devInstall.bat
@@ -0,0 +1,6 @@
+# INSTALL CHOCOLATERY
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+
+
+# INSTALL MARCUS DEV APPS 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.github.com/marcusoftnet/ScriptsAndStuff/master/devApps.ps1'))" 
