4b03ced95d1796ecfe4093636103c5930ca3c3d8
Marcus Hammarberg <marcusoft.net@gmail.com>
2013-08-01 20:32:14 +0200

    Enable all of them
    Next run will take awhile... A long while.

--- DevMachineInstall/devApps.ps1
+++ DevMachineInstall/devApps.ps1
@@ -1,24 +1,25 @@
 # Dev apps to install
 cinstm notepadplusplus
 cinstm Firefox
-#cinstm winmerge
-#cinstm git
-#cinstm resharper -Version 7.1.3000.2254
+cinstm winmerge
+cinstm Console2
+cinstm git
+cinstm resharper -Version 7.1.3000.2254
 cinstm 7zip
-#cinstm nodejs.install
+cinstm nodejs.install
 cinstm fiddler
-#cinstm GoogleChrome
-#cinstm curl
-#cinstm linqpad4
-#cinstm mongodb
-#cinstm MongoVUE
-#cinstm P4Merge
-#cinstm Growl 
-#cinstm pidgin
-#cinstm typescript
-#cinstm typescript.vs
-#cinstm VisualStudio2013Ultimate
-#cinstm VisualStudio2012Ultimate
-#cinstm WebEssentials2012
-#cinstm MsSqlServer2012Express
-#cinstm xUnit.vs2012
+cinstm GoogleChrome
+cinstm curl
+cinstm linqpad4
+cinstm mongodb
+cinstm MongoVUE
+cinstm P4Merge
+cinstm Growl 
+cinstm pidgin
+cinstm typescript
+cinstm typescript.vs
+cinstm VisualStudio2013Ultimate
+cinstm VisualStudio2012Ultimate
+cinstm WebEssentials2012
+cinstm MsSqlServer2012Express
+cinstm xUnit.vs2012
