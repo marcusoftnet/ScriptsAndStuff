5c8b6c3628f02097e6295f71af6d8f38167ed32d
Marcus Hammarberg <marcusoft.net@gmail.com>
2013-08-02 09:54:22 +0200

    Reordered and grouped the apps
    Ha - turns out it's a great idea to install Visual Studio before you install add-ins to it. Who knew? :P

--- DevMachineInstall/devApps.ps1
+++ DevMachineInstall/devApps.ps1
@@ -1,22 +1,33 @@
 # Dev apps to install
-cinstm notepadplusplus
+
+# Visual studio stuff
+cinstm VisualStudio2013Ultimate
+cinstm VisualStudio2012Ultimate
+
+# Visual studio add-ins
+cinstm xUnit.vs2012
+cinstm WebEssentials2012
+cinstm resharper -Version 7.1.3000.2254
+cinstm typescript
+cinstm typescript.vs
+
+# databases and tools
+cinstm MsSqlServer2012Express
+cinstm mongodb
+cinstm MongoVUE
+
+# browsers
 cinstm Firefox
+cinstm GoogleChrome
+
+# Other tools
+cinstm git
+cinstm notepadplusplus
 cinstm winmerge
 cinstm Console2
-cinstm git
-cinstm resharper -Version 7.1.3000.2254
 cinstm 7zip
 cinstm nodejs.install
 cinstm fiddler
-cinstm GoogleChrome
 cinstm curl
 cinstm linqpad4
-cinstm mongodb
-cinstm MongoVUE
-cinstm typescript
-cinstm typescript.vs
-cinstm VisualStudio2013Ultimate
-cinstm VisualStudio2012Ultimate
-cinstm WebEssentials2012
-cinstm MsSqlServer2012Express
-cinstm xUnit.vs2012
+
