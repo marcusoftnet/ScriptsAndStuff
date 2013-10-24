7d7bf0e9079617154c88478223bccb508c11eb95
Marcus Hammarberg <marcusoft.net@gmail.com>
2013-10-24 09:59:26 +0200

    Reinstall inital

---
+++ DevMachineInstall/devAppsReinstall.ps1
@@ -0,0 +1,39 @@
+# Dev apps to install
+
+# Visual studio stuff
+cinst VisualStudio2013Ultimate -force
+cinst VisualStudio2012Ultimate -force
+
+# Visual studio add-ins and stuff
+cinst xUnit.vs2012 -force
+cinst WebEssentials2012 -force
+cinst resharper -Version 7.1.3000.2254  -force # Use only resharper if you want the latests. This is what I have a license for
+cinst typescript  -force
+cinst typescript.vs -force
+cinst SpecFlow -force
+
+# databases and tools
+cinst MsSqlServer2012Express  -force
+cinst mongodb -force
+cinst MongoVUE -force
+
+# browsers
+cinst Firefox -force
+cinst GoogleChrome -force
+
+# Other tools
+cinst git -force
+cinst gitextensions -force
+cinst githubforwindows -force
+cinst notepadplusplus -force
+cinst winmerge -force
+cinst Console2 -force
+cinst 7zip -force
+cinst nodejs.install -force
+cinst fiddler -force
+cinst curl -force
+cinst linqpad4 -force
+cinst scriptcs -force
+cinst sublimetext2 -force
+cinst SublimeText2.PackageControl -force
+cinst ccleaner -force
